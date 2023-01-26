f0086f068890cdb42bf54f92a938a18229f40b66
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-01-26 15:06:33 +0100

    improve form validator

--- public/js/validate-signup.js
+++ public/js/validate-signup.js
@@ -1,18 +1,43 @@
 const form = document.querySelector("form");
+const usernameInput = form.querySelector('input[name="username"]');
 const emailInput = form.querySelector('input[name="email"]');
 const passwordInput = form.querySelector('input[name="password"]');
 const passwordRepeatedInput = form.querySelector('input[name="passwordRepeated"]');
+const submitButton = form.querySelector('.submit-btn');
+
+function isUsernameValid(username) {
+    return username !== '' && username.length < 64;
+}
 
 function isEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
+}
+
+function isPasswordStrong(password) {
+    return /^(?=.*[a-z])(?=.*[A-Z]).{8,64}$/.test(password);
 }
 
 function arePasswordsSame(password, passwordRepeated) {
     return password === passwordRepeated;
 }
 
+function isFormValid() {
+    return isUsernameValid(usernameInput.value) &&
+        isEmail(emailInput.value) &&
+        isPasswordStrong(passwordInput.value) &&
+        arePasswordsSame(passwordInput.value, passwordRepeatedInput.value);
+}
+
 function markValidation(element, condition) {
     !condition ? element.classList.add('no-valid') : element.classList.remove('no-valid');
+}
+
+function validateUsername() {
+    setTimeout(function () {
+            markValidation(usernameInput, isUsernameValid(usernameInput.value));
+        },
+        1000
+    );
 }
 
 function validateEmail() {
@@ -25,15 +50,35 @@ function validateEmail() {
 
 function validatePassword() {
     setTimeout(function () {
-            const condition = arePasswordsSame(
-                passwordInput.value,
-                passwordRepeatedInput.value
-            );
-            markValidation(passwordRepeatedInput, condition);
+            markValidation(passwordInput, isPasswordStrong(passwordInput.value));
         },
         1000
     );
 }
 
+function validatePasswordRepeated() {
+    setTimeout(function () {
+            markValidation(
+                passwordRepeatedInput,
+                arePasswordsSame(passwordInput.value, passwordRepeatedInput.value)
+            );
+        },
+        1000
+    );
+}
+
+function validateForm() {
+    isFormValid() ? submitButton.classList.remove("disabled") : submitButton.classList.add("disabled");
+}
+
+validateForm();
+
+usernameInput.addEventListener('keyup', validateUsername);
 emailInput.addEventListener('keyup', validateEmail);
-passwordRepeatedInput.addEventListener('keyup', validatePassword);
+passwordInput.addEventListener('keyup', validatePassword);
+passwordRepeatedInput.addEventListener('keyup', validatePasswordRepeated);
+
+usernameInput.addEventListener('keyup', validateForm);
+emailInput.addEventListener('keyup', validateForm);
+passwordInput.addEventListener('keyup', validateForm);
+passwordRepeatedInput.addEventListener('keyup', validateForm);
